1190d42273c0d93ce64e4e4a4fcf3208d869fc00
ruslantuchkov <nidjel87@gmail.com>
2018-04-06 17:49:36 +0500

    added some dynamic to Burger comp

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,12 +2,23 @@ import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const Burger = () => {
+const Burger = ({ ingredients }) => {
+  let arrOfIngredients = Object.keys(ingredients)
+    .map(ing =>
+      [...Array(ingredients[ing])].map((_, i) => (
+        <BurgerIngredient key={ing + i} type={ing} />
+      ))
+    )
+    .reduce((arr, elem) => [...arr, ...elem], []);
+
+  if (arrOfIngredients.length === 0) {
+    arrOfIngredients = <p>Please start adding ingredients.</p>;
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {arrOfIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
