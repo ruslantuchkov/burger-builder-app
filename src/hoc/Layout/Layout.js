5ba1ff673548d5057641ca2d6a115acf536644f2
ruslantuchkov <nidjel87@gmail.com>
2018-04-07 13:18:06 +0500

    building Navigation

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import Aux from '../Aux';
 import classes from './Layout.css';
+import Aux from '../Aux';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
   render() {
     return (
       <Aux>
-        <div>Toolbar, Sidebar, Backdrop</div>
+        <Toolbar />
+        <SideDrawer />
         <main className={classes.Content}>{this.props.children}</main>
       </Aux>
     );
